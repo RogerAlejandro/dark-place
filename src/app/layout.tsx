99e5e0630f99462e1dc59bf5526e6f682a4684af
RogerAlejandro <roller.alesandro@gmail.com>
2025-07-02 16:51:08 -0500

    2.4.1 mod de metadato + métricas en vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from 'next/font/google';
 import "./globals.css";
 import { BackgroundProvider } from "@/contexts/BackgroundContext";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -15,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'Dark Place - Fondos Oscuros',
-  description: 'Colección de fondos oscuros modernos para tus proyectos web',
+  description: 'Colección de fondos oscuros inspirados en el espacio y universo 🚀🌒, para probar en sitios web ',
 }
 
 export default function RootLayout({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0A0A0A] text-white min-h-screen`}>
         <BackgroundProvider>
           {children}
+          <Analytics />
         </BackgroundProvider>
       </body>
     </html>
